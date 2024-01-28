c1f06324421c04fa5ba098b93f69da17fd40478a
SatishCambrian <156278516+SatishCambrian@users.noreply.github.com>
2024-01-28 20:31:49 +0000

    Test added

---
+++ TestApi/TestController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyApp.Namespace
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        public string TestMethod()
+        {
+            return "Testing0";
+        }
+
+        public string Get()
+        {
+            return "Get Method";
+        }
+    }
+}
